5d7faf6b2ab0052a0edaff42bdcee9a9bb9d14a5
Moary Chen <v-shilichen@microsoft.com>
2024-08-07 12:52:38 +0800

    Add Spring Boot Admin for local dev

--- samples/spring-petclinic-microservices/spring-petclinic-microservices-apphost/src/main/java/com/azure/example/petclinic/PetClinicAppHost.java
+++ samples/spring-petclinic-microservices/spring-petclinic-microservices-apphost/src/main/java/com/azure/example/petclinic/PetClinicAppHost.java
@@ -22,35 +22,31 @@ public class PetClinicAppHost implements DcpAppHost {
                 .addEurekaServiceDiscovery("eureka");
         
         SpringProject configServer = spring.addSpringProject("spring-petclinic-config-server")
-                .withHttpEndpoint(8888)
                 .withExternalHttpEndpoints();
 
         spring.addSpringProject("spring-petclinic-customers-service")
                 .withDependency(configServer)
                 .withDependency(discoveryServer)
-                .withEnvironment("SERVER_PORT", "8081")
-                .withEnvironment("SPRING_APPLICATION_NAME", "customers-service")
                 .withReference(discoveryServer);
 
         spring.addSpringProject("spring-petclinic-vets-service")
                 .withDependency(configServer)
                 .withDependency(discoveryServer)
-                .withEnvironment("SERVER_PORT", "8082")
-                .withEnvironment("SPRING_APPLICATION_NAME", "vets-service")
                 .withReference(discoveryServer);
 
         spring.addSpringProject("spring-petclinic-visits-service")
                 .withDependency(configServer)
                 .withDependency(discoveryServer)
-                .withEnvironment("SERVER_PORT", "8083")
-                .withEnvironment("SPRING_APPLICATION_NAME", "visits-service")
                 .withReference(discoveryServer);
 
         spring.addSpringProject("spring-petclinic-api-gateway")
                 .withDependency(configServer)
                 .withDependency(discoveryServer)
-                .withEnvironment("SERVER_PORT", "8080")
-                .withEnvironment("SPRING_APPLICATION_NAME", "api-gateway")
+                .withReference(discoveryServer);
+
+        spring.addSpringProject("spring-petclinic-admin-server")
+                .withDependency(configServer)
+                .withDependency(discoveryServer)
                 .withReference(discoveryServer);
     }
 }
